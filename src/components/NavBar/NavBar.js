5ff599d0f075e937155a2818ade9c40358ad0689
Shahriar Swim <shahriar2k18@gmail.com>
2021-04-09 15:39:05 +0600

    ✅ navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,111 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Heading } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+// css
+import "./NavBar.css";
 
 const NavBar = () => {
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth <= 1000 ? true : false
+  );
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const onResizeHandler = () => {
+    console.log(window.innerWidth);
+    if (window.innerWidth <= 1000) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+  useEffect(() => {
+    window.onresize = onResizeHandler;
+  }, [window.onresize]);
+
   return (
-    <nav>
-      <Heading>Eventman</Heading>
-    </nav>
+    <>
+      <nav className="navBar">
+        <Heading size="xl">
+          <NavLink to="/">Eventman</NavLink>
+        </Heading>
+        {!isMobile && (
+          <ul className="navBar__list desktop">
+            <li className="navBar__list-item">
+              <NavLink to="/" exact>
+                Home
+              </NavLink>
+            </li>
+            <li className="navBar__list-item">
+              <NavLink to="/services">Services</NavLink>
+            </li>
+            <li className="navBar__list-item">
+              <NavLink to="/#about">About</NavLink>
+            </li>
+            <li className="navBar__list-item">
+              <NavLink to="/#contact">Contact</NavLink>
+            </li>
+            <li className="navBar__list-item">
+              <NavLink to="/auth">Login/Register</NavLink>
+            </li>
+          </ul>
+        )}
+        {isMobile && (
+          <button
+            className="mobileNav__btn"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <HiMenuAlt3 />
+          </button>
+        )}
+      </nav>
+      {isMenuOpen && (
+        <div className="mobileNav">
+          <button
+            className="mobileNav__btn-close"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            <AiOutlineClose />
+          </button>
+          <ul className="mobileNav__list">
+            <li
+              className="mobileNav__list-item"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <NavLink to="/" exact>
+                Home
+              </NavLink>
+            </li>
+            <li
+              className="mobileNav__list-item"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <NavLink to="/services">Services</NavLink>
+            </li>
+            <li
+              className="mobileNav__list-item"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <NavLink to="/#about">About</NavLink>
+            </li>
+            <li
+              className="mobileNav__list-item"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <NavLink to="/#contact">Contact</NavLink>
+            </li>
+            <li
+              className="mobileNav__list-item"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <NavLink to="/auth">Login/Register</NavLink>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
