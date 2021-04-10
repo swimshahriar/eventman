cc110954fb648df72b94118e0cde544ffd27ded1
Shahriar Swim <shahriar2k18@gmail.com>
2021-04-10 23:44:20 +0600

    ✅ about section

---
+++ src/components/AboutSection/AboutSection.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Heading, Image, Text } from "@chakra-ui/react";
+
+// assets
+import AboutSvg from "../../assets/home/about.svg";
+
+// styles
+import "./AboutSection.css";
+
+const AboutSection = () => {
+  return (
+    <div className="globalPadding about">
+      <Heading as="h2" size="2xl" textAlign="center">
+        About Us
+      </Heading>
+      <div className="about__detailContainer">
+        <Image
+          src={AboutSvg}
+          alt="about us"
+          className="about__detailContainer-img"
+        />
+        <Text className="about__detailContainer-text" fontSize="xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eos
+          deserunt corrupti deleniti dolores pariatur sapiente illo ut sint
+          atque ducimus quia sed nulla, libero cumque quasi. Magni, atque
+          obcaecati!
+        </Text>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 
 //components
 import HeroSection from "../components/HeroSection/HeroSection";
+import AboutSection from "../components/AboutSection/AboutSection";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <AboutSection />
     </>
   );
 };
