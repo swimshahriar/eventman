273057f8636fe2fc31fe3f15a9d0a287cc616938
Shahriar Swim <shahriar2k18@gmail.com>
2021-04-14 15:53:03 +0600

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: 'eventman-project',
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
